7ab39045b3d595590634d84f635994bc9b8e10e2
dhavalthakur11 <dhavalthakor88551@gmail.com>
2024-06-16 22:13:21 +0530

    updated

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const expressError = require("./utils/expressError.js");
-const listings = require("./routs/listing.js");
-const reviews = require("./routs/reviews.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStategy = require("passport-local");
+const User = require("./models/users.js");
+
+const listingsRouter = require("./routs/listing.js");
+const reviewsRouter = require("./routs/reviews.js");
+const userRouter = require("./routs/user.js");
 
 
 app.set("view engine", "ejs");
@@ -19,6 +24,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+main().then(() => {
+    console.log("connected");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
 
 const sessionOption = {
     secret:"mysupersecretcode",
@@ -38,26 +54,32 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOption));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
-main().then(() => {
-    console.log("connected");
-}).catch((err) => {
-    console.log(err);
-});
+// app.get("/random",async(req,res)=>{
+//     let fakeUser = new User ({
+//         email:"dhaval@gmail.com",
+//         username:"dhaval"
+//     });
+//     let registerUser = await User.register(fakeUser,"dhaval@123");
+//     res.send(registerUser);
+// })
 
-async function main() {
-    await mongoose.connect(MONGO_URL);
-}
-
-
-
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/",userRouter);
 
 app.all("*", (req, res, next) => {
     next(new expressError(404, "Page Not Found!"));
---
+++ routs/user.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/users.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+
+
+router.get("/singup",(req,res)=>{
+    res.render("users/singup.ejs");
+});
+
+router.post("/singup",wrapAsync(async(req,res)=>{
+    try{
+        let {username,email,password} = req.body;
+        const newUser = new User({email,username});
+        const registeredUser = await User.register(newUser,password);
+        console.log(registeredUser);
+        req.flash("success","welcome to wanderlust");
+        res.redirect("/listings");
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/singup");
+    }  
+}));
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login",passport.authenticate("local",{failureRedirect:'/login',failureFlash:true}),async(req,res)=>{ 
+//passport.authenticate it's use for authenticate user from database it's  exit or no
+    req.flash("success","welcome to wanderlust. you are loged in");
+    res.redirect("/listings");
+});
+
+module.exports = router;
